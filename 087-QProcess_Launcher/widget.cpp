ea066405c643b3986d157f9151bf17b2a890dded
Victor <vicfer89@gmail.com>
2019-02-07 16:35:24 +0100

    La aplicación colapsa al ejecutar un fichero.

--- 087-QProcess_Launcher/widget.cpp
+++ 087-QProcess_Launcher/widget.cpp
@@ -1,14 +1,84 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QSystemTrayIcon>
+#include <QInputDialog>
+#include <QFileDialog>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+   // mTrayIcon->setVisible(true);
+    //mTrayIcon->setIcon(QIcon);
+    //mTrayIcon->show();
+    makeConnections();
+
+    ui->argumentos_listWidget->addActions({ui->actionAgregarArgumento,
+                                           ui->actionEliminarArgumento });
+
 }
 
 Widget::~Widget()
 {
     delete ui;
+}
+
+void Widget::makeConnections()
+{
+    auto finishedSignal =  static_cast<void (QProcess::*)(int)>(&QProcess::finished);
+    connect(&mProcess, finishedSignal, [&](int exitCode){
+        mTrayIcon->showMessage(tr("Mensaje"), tr("Proceso terminado...: %1").arg(exitCode));
+    });
+
+    connect(&mProcess, &QProcess::started, [&](){
+        mTrayIcon->showMessage(tr("Mensaje"), tr("Proceso iniciado..."));
+    });
+}
+
+void Widget::on_actionAgregarArgumento_triggered()
+{
+    bool ok;
+    auto args = QInputDialog::getMultiLineText(this, tr("Argumento"), tr("Argumento"),"",&ok);
+    if(!ok || args.isEmpty()) {
+       return;
+    }
+    else {
+        ui->argumentos_listWidget->addItem(new QListWidgetItem(args));
+    }
+}
+
+void Widget::on_actionEliminarArgumento_triggered()
+{
+    delete ui->argumentos_listWidget->currentItem();
+}
+
+void Widget::on_ejecutar_pushButton_clicked()
+{
+    auto program = ui->program_lineEdit->text();
+    const int argc = ui->argumentos_listWidget->count();
+    QStringList args;
+    for(int i = 0; i < argc; ++i)
+    {
+        args << ui->argumentos_listWidget->item(i)->text();
+    }
+    mProcess.setProgram(program);
+    mProcess.setArguments(args);
+    mProcess.start();
+}
+
+void Widget::on_salir_pushButton_clicked()
+{
+    close();
+}
+
+void Widget::on_toolButton_clicked()
+{
+    auto filename = QFileDialog::getOpenFileName(this, tr("Abrir"),"/", "EXE files (*.exe)");
+    if(filename.isEmpty()){
+        return;
+    }
+    else {
+        ui->program_lineEdit->setText(filename);
+    }
 }
--- 087-QProcess_Launcher/widget.h
+++ 087-QProcess_Launcher/widget.h
@@ -2,10 +2,13 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QProcess>
 
 namespace Ui {
 class Widget;
 }
+
+class QSystemTrayIcon;
 
 class Widget : public QWidget
 {
@@ -15,8 +18,23 @@ public:
     explicit Widget(QWidget *parent = nullptr);
     ~Widget();
 
+private slots:
+    void on_actionAgregarArgumento_triggered();
+
+    void on_actionEliminarArgumento_triggered();
+
+    void on_ejecutar_pushButton_clicked();
+
+    void on_salir_pushButton_clicked();
+
+    void on_toolButton_clicked();
+
 private:
+    void makeConnections();
+
     Ui::Widget *ui;
+    QProcess mProcess;
+    QSystemTrayIcon *mTrayIcon;
 };
 
 #endif // WIDGET_H
